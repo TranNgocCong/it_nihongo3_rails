3693f830c38efd548d4ddabcc1189eb287f4605b
Ubuntu <ubuntu@ip-172-31-59-188.ec2.internal>
2020-11-22 13:54:07 +0000

    Add ava for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
                devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name,:age, :email, :password)}
 
                devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name,:age, :email, :password, :current_password)}
+               
+               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name,:age, :email, :password, :avatar)}
+               
+               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name,:age, :email, :password, :current_password, :avatar)}
           end
 
 
